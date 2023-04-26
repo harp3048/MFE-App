0ef7f14743cd31ba63fb0f624f684b26c856a7e0
Harpreet Singh <harpreet.singh@rackspace.com>
2023-04-26 10:51:54 -0500

    updated text

--- src/App3Component.tsx
+++ src/App3Component.tsx
@@ -4,8 +4,8 @@ import "./App.css";
 export default function App3Component() {
 
     return (
-      <div className='app3component'>
-        <p>This component is from App3</p>
+      <div className="app3component">
+        <p>This component is from App3 (Deployed at AWS S3)</p>
         <button onClick={() => alert("App3 Button clicked")}>
           App3 Button
         </button>
