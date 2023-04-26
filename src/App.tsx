6b61a9ab1fc552f15544bb1b8b851c0452303714
Harpreet Singh <harpreet.singh@rackspace.com>
2023-04-26 09:39:13 -0500

    css updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import App3Component from './App3Component';
 
 function App() {
   return (
-    <div>
+    <div style={{ marginLeft: "50px" }}>
+      <h1 style={{ marginLeft: "20px" }}>App3</h1>
       <App3Component />
     </div>
   );
